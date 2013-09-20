361baaf32eaac883c62bd9efed1270c2a1a0fbb2
jgronost <jgronost@gmail.com>
2013-09-20 13:15:27 +0200

    session added

--- progfun-example/src/main/scala/example/Lists.scala
+++ progfun-example/src/main/scala/example/Lists.scala
@@ -23,7 +23,12 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+	  xs match {
+	    case x :: tail => x + sum(tail) // if there is an element, add it to the sum of the tail
+	    case Nil => 0 // if there are no elements, then the sum is 0
+	  }
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +43,11 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    if (xs.isEmpty) 0
+    else {
+      if( xs.head >= max(xs.tail) ) xs.head
+      else max(xs.tail)
+    }
+}
 }
