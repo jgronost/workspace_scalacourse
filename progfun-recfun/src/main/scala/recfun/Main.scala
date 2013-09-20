5fc68fafa8a92cb334de292053c7c47cd8777055
jgronost <jgronost@gmail.com>
2013-09-20 16:45:24 +0200

    week1 solution

--- progfun-recfun/src/main/scala/recfun/Main.scala
+++ progfun-recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,49 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c==r) 1
+    else if (r>0 && c== 0) 1
+    else (pascal(c-1,r-1) + pascal(c,r-1))
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    	def balance2 (chars: List[Char], counter: Int): Boolean = {
+    	    	if (chars.isEmpty  && counter == 0) true
+			    	else if (chars.isEmpty && counter != 0) false
+			    	else {
+				    	var char = chars.head
+				    	if (char == '(' ){
+				    		balance2(chars.tail, counter +1 );
+				    	}
+				    	else if(char == ')' ) {
+				    		if (counter ==0) false
+				    		else balance2(chars.tail, counter -1);
+				    	}
+				    	else  balance2(chars.tail, counter);
+				}
+    	}
+    	balance2(chars, 0)
+    }                                        
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+		 def calulateCombinations(money: Int, coins: List[Int]): Int = {
+		 		if(money == 0) 1
+		 		else if (money < 0 || coins.isEmpty) 0
+		 		else {
+		 			calulateCombinations(money - coins.head, coins) +
+		 			calulateCombinations(money, coins.tail)
+		 		}
+		 }
+		 
+		 if (money == 0) 0
+		 else calulateCombinations(money, coins)
+		
+		} 
 }
